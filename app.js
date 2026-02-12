a4ca02fe0782ec65dad3f3a61458f2abeebcc2c0
sakshi-mahajan27 <sakshimahajan270604@gmail.com>
2026-02-12 23:13:12 +0530

    Added cookies, session and flash messages

--- app.js
+++ app.js
@@ -5,6 +5,26 @@ const path=require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError=require("./utils/ExpressError.js")
+const session=require("express-session");
+const flash=require("connect-flash");
+
+const sessionOptions={
+    secret:"mysupersecretcode",
+    resave:false,
+    saveUninitialized:true,
+    cookie:{
+      expires:Date.now() + 1000 * 60 * 60 * 24 * 7,
+      maxAge:1000 * 60 * 60 * 24 * 7,
+      httpOnly:true, //to prevent cross site scripting attacks, it will not allow client side js to access the cookie
+    },
+};
+
+app.get("/", (req, res) => {
+    res.send("Hi");
+});
+
+app.use(session(sessionOptions));
+app.use(flash());
 
 const listings = require("./routes/listing.js");
 const reviews = require("./routes/review.js");
@@ -19,10 +39,6 @@ async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/urbanvista');
 }
 
-app.get("/", (req, res) => {
-    res.send("Hi");
-});
-
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname,  "views"));
 app.use(express.urlencoded({ extended: true }));
@@ -30,8 +46,13 @@ app.use(methodOverride("_method"));
 app.engine('ejs',ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 
-app.use("/listings", listings);
+app.use((req, res, next) => {
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    next();
+});
 
+app.use("/listings", listings);
 app.use("/listings/:id/reviews", reviews);
 
 app.all(/.*/, (req,res,next) => {
