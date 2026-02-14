76038e777b967f0ca5a68505314dfe9664171f2a
sakshi-mahajan27 <sakshimahajan270604@gmail.com>
2026-02-14 09:33:50 +0530

    Added Login Feature

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
+const passport = require("passport");
 
 router.get("/signup", (req, res) => {
     res.render("users/signup.ejs");
@@ -20,5 +21,17 @@ router.post("/signup", wrapAsync(async (req, res) => {
         res.redirect("/signup");
     }
 }));
+
+router.get("/login", (req, res) => {
+    res.render("users/login.ejs");
+});
+
+router.post("/login", passport.authenticate("local", {
+    failureFlash: true,
+    failureRedirect: "/login",
+}), async (req, res) => {
+    req.flash("success", "Welcome back!");
+    res.redirect("/listings"); 
+});
 
 module.exports = router;
