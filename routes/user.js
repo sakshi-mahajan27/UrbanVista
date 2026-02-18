36e30d97d9efe1f4c7047c1140d64677c20b7e29
Sakshi Mahajan <sakshimahajan270604@gmail.com>
2026-02-18 20:10:12 +0530

    Changed rating and routes

--- routes/user.js
+++ routes/user.js
@@ -5,13 +5,14 @@ const passport = require("passport");
 const {saveRedirectUrl} = require("../middleware.js");
 const userController = require("../controllers/users.js");
 
-router.get("/signup", userController.renderSignUpForm);
+router.route("/signup")
+    .get(userController.renderSignUpForm)
+    .post(wrapAsync(userController.signup));
 
-router.post("/signup", wrapAsync(userController.signup));
 
-router.get("/login", userController.renderLoginForm);
-
-router.post("/login",saveRedirectUrl, passport.authenticate("local", {
+router.route("/login")
+    .get(userController.renderLoginForm)
+    .post(saveRedirectUrl, passport.authenticate("local", {
     failureFlash: true,
     failureRedirect: "/login",
 }), userController.login);
